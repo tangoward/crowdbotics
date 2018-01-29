016b38626f4feede55bb4941bf11839902715bc5
Jarvis <tangoward15@gmail.com>
2018-01-29 18:28:04 +0800

    Setting STATIC

--- pets/settings.py
+++ pets/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = conifg('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
